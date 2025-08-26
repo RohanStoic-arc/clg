b3d84a2b5a1cff43c76ced79b05b34513f1480bf
RohanStoic-arc <rohan.stoic01@gmail.com>
2025-08-26 11:34:55 +0530

    Update c.c

--- c.c
+++ c.c
@@ -1,1 +1,9 @@
 // wap that will print your mailing address in the following form 
+// wap that will print your mailing address in the following form 
+// given the radius of a circle write a program to compute and display its area 
+//given the values of three variables a,b,and c wap to compute and display the values of x
+// where x=a/b - c
+
+// relationship between celcius and fahrenhiet is govern by the formula f=9c/5 + 32;
+// wap to convert the temprature from celcius to fahrenheit 
+// from f to celcius
