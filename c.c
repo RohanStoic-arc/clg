520bdaabeaaa2e7ce8b1a0b21a104080968ad131
RohanStoic-arc <rohan.stoic01@gmail.com>
2025-08-26 11:37:13 +0530

    Update c.c

--- c.c
+++ c.c
@@ -1,5 +1,4 @@
 // wap that will print your mailing address in the following form 
-// wap that will print your mailing address in the following form 
 // given the radius of a circle write a program to compute and display its area 
 //given the values of three variables a,b,and c wap to compute and display the values of x
 // where x=a/b - c
@@ -7,3 +6,6 @@
 // relationship between celcius and fahrenhiet is govern by the formula f=9c/5 + 32;
 // wap to convert the temprature from celcius to fahrenheit 
 // from f to celcius
+// area of triangle is given by a= sqroot(s(s-a)(s-b(s-c)))) where a b c are the side of 
+//triangle and 2s=a+b+c
+//wap to compute the area of triangle given the values if a b and c 
